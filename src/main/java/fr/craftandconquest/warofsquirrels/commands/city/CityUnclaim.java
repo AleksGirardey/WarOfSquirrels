b281eedc84e592151e924e617bc3ce77816e0c0a
Alexis GIRARDEY <alexis@pinpinteam.com>
2021-03-08 19:07:10 +0100

    feat: wip city command

--- src/main/java/fr/craftandconquest/warofsquirrels/commands/city/CityUnclaim.java
+++ src/main/java/fr/craftandconquest/warofsquirrels/commands/city/CityUnclaim.java
@@ -6,6 +6,7 @@ import fr.craftandconquest.warofsquirrels.WarOfSquirrels;
 import fr.craftandconquest.warofsquirrels.handler.ChunkHandler;
 import fr.craftandconquest.warofsquirrels.object.Player;
 import fr.craftandconquest.warofsquirrels.object.faction.city.CityRank;
+import fr.craftandconquest.warofsquirrels.object.world.Chunk;
 import fr.craftandconquest.warofsquirrels.object.world.ChunkLocation;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
@@ -16,16 +17,44 @@ public class CityUnClaim extends CityAssistantCommandBuilder {
 
     @Override
     public LiteralArgumentBuilder<CommandSource> register() {
-        return null;
+        return Commands.literal("unclaim").executes(this);
     }
 
     @Override
     protected boolean SpecialCheck(Player player, CommandContext<CommandSource> context) {
-        return false;
+        int x = player.getPlayerEntity().chunkCoordX;
+        int z = player.getPlayerEntity().chunkCoordZ;
+        int dimensionId = player.getPlayerEntity().dimension.getId();
+
+        Chunk chunk = WarOfSquirrels.instance.getChunkHandler().getChunk(x, z, dimensionId);
+        StringTextComponent message = new StringTextComponent("");
+
+        boolean         ret = true;
+
+        if (chunk == null || chunk.getCity() != player.getCity()) {
+            message.appendText("This chunk did not belong to your city.").applyTextStyle(TextFormatting.RED);
+            ret = false;
+        } else if (chunk.getHomeBlock()) {
+            message.appendText("You cannot un-claim your HomeBlock");
+            ret = false;
+        }
+
+        player.getPlayerEntity().sendMessage(message);
+        return ret;
     }
 
     @Override
     protected int ExecCommand(Player player, CommandContext<CommandSource> context) {
+        int x = player.getPlayerEntity().chunkCoordX;
+        int z = player.getPlayerEntity().chunkCoordZ;
+        int dimensionId = player.getPlayerEntity().dimension.getId();
+
+        Chunk chunk = WarOfSquirrels.instance.getChunkHandler().getChunk(x, z, dimensionId);
+
+        WarOfSquirrels.instance.getChunkHandler().Delete(chunk);
+        StringTextComponent message = Text.of("La parcelle " + chunk.toString() + " a été libérée de tout contrôle.");
+        Core.getInfoCityMap().get(player.getCity()).getChannel().send(
+                Text.of(TextColors.GREEN, message, TextColors.RESET));
         return 0;
     }
 }
